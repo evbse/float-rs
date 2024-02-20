97189e334c5e9eef48ecc4345507f72af3da7098
evbse <evbse@proton.me>
2024-02-20 11:50:57 +0200

    stress test to/from bytes our implementation

--- src/bin/stress.rs
+++ src/bin/stress.rs
@@ -1,6 +1,5 @@
 extern crate float;
 
-// use core::ffi::CStr;
 use std::thread::spawn;
 
 fn main() {
@@ -10,19 +9,19 @@ fn main() {
     let mut threads = Vec::with_capacity((NTHREADS - 1) as usize);
     for i in 0..NTHREADS - 1 {
         threads.push(spawn(move || {
-            // let mut b = float::to_bytes::f32::Buffer::new();
-            let mut b = Vec::<u8>::with_capacity(32);
+            let mut b = float::to_bytes::f32::Buffer::new();
+            // let mut b = Vec::<u8>::with_capacity(32);
             for j in STEP * i..STEP * (i + 1) {
                 let f = f32::from_bits(j);
                 if f.is_finite() {
                     let a: f32 = unsafe { core::mem::transmute(j) };
-                    // let s = b.format(a);
-                    let len = float::ffi::to_bytes_f32_c(b.as_mut_ptr().cast(), f);
-                    unsafe { b.set_len(len) };
+                    let s = b.format(a);
+                    // let len = float::ffi::to_bytes_f32_c(b.as_mut_ptr().cast(), f);
+                    // unsafe { b.set_len(len) };
                     // let b: f32 = float::ffi::from_bytes_f32_c(unsafe {
                     //     CStr::from_bytes_with_nul_unchecked(s.as_bytes())
                     // });
-                    let b = float::from_bytes::parse::<f32>(&b);
+                    let b = float::from_bytes::parse::<f32>(s.as_bytes());
                     assert!(a == b);
                 }
             }
@@ -30,19 +29,19 @@ fn main() {
     }
 
     let i = NTHREADS - 1;
-    // let mut b = float::to_bytes::f32::Buffer::new();
-    let mut b = Vec::<u8>::with_capacity(32);
+    let mut b = float::to_bytes::f32::Buffer::new();
+    // let mut b = Vec::<u8>::with_capacity(32);
     for j in STEP * i..=u32::MAX {
         let f = f32::from_bits(j);
         if f.is_finite() {
             let a: f32 = unsafe { core::mem::transmute(j) };
-            // let s = b.format(a);
-            let len = float::ffi::to_bytes_f32_c(b.as_mut_ptr().cast(), f);
-            unsafe { b.set_len(len) };
+            let s = b.format(a);
+            // let len = float::ffi::to_bytes_f32_c(b.as_mut_ptr().cast(), f);
+            // unsafe { b.set_len(len) };
             // let b: f32 = float::ffi::from_bytes_f32_c(unsafe {
             //     CStr::from_bytes_with_nul_unchecked(s.as_bytes())
             // });
-            let b = float::from_bytes::parse::<f32>(&b);
+            let b = float::from_bytes::parse::<f32>(s.as_bytes());
             assert!(a == b);
         }
     }
